b08781fa7f7674335a958faf31aac646e7f32076
profa-janaina <111704392+profa-janaina@users.noreply.github.com>
2023-05-10 17:26:08 -0300

    main.js

--- main.js
+++ main.js
@@ -4,20 +4,10 @@ var canvas = new fabric.Canvas('myCanvas');
 function newImage()
 {
 	
-	fabric.Image.fromURL('BirthdayImage.jpg', function(Img) {
-	blockImageObject = Img;
-		
-	blockImageObject.scaleToWidth(700);
-	blockImageObject.scaleToHeight(510);
-	blockImageObject.set({
-	top:0,
-	left:0
-	});
-	canvas.add(blockImageObject);
-	});
+	// complete o código
 	
 }
 
 function playSound(){
-	x.play();
+	// complete o código
 }
